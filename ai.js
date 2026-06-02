811afefe3249f4f789707dc78b17aabe0277fe9c
DaAppleTree <theamazing.appletree@gmail.com>
2026-06-01 23:09:40 -0400

    added greedy algorithm for AI
    implemented a simple greedy algorithm for determining the best local move with the given cards

---
+++ ai.js
@@ -0,0 +1,32 @@
+// function that scores how good a move is depending on the old and new values
+function score(newVal, oldVal, target) {
+    oldScore = Math.abs(oldVal - target)
+    newScore = Math.abs(newVal - target)
+    return newScore - oldScore;
+}
+
+// greedy function that makes the AI choose the best local move
+function chooseMove(hand, aiVal, playerVal, target){
+    // set best score as low as possible, and best move to a random arbritary move
+    let bestScore = -Infinity
+    let bestMove = {cardIndex: 0, applyTo: "self"};
+    hand.forEach((card, i) => {
+        // for each card, check how much closer the AI can be to the target
+        const newAiVal = card.fn(aiVal);
+        const selfScore = score(newAiVal, aiVal, target);
+        if (selfScore > bestScore){
+            bestScore = selfScore;
+            bestMove = {cardIndex: i, applyTo: "self"};
+        }
+
+        // for each card, check how much farther the player can be to the target
+        const newPlayerVal = card.fn(playerVal);
+        const otherScore = -score(newPlayerVal, playerVal, target);
+        if (otherScore > bestScore){
+            bestScore = otherScore;
+            bestMove = {cardIndex: i, applyTo: "player"};
+        }
+    });
+    // return the best move out of all these options
+    return bestMove;
+}
